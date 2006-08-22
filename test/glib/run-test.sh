368aaee38049dc599fb8d06d7096bcdbd04befab
Steve Frécinaux <steve@istique.net>
2006-08-22 22:26:24 +0200

    Make test scripts run during out-of-tree compilation

--- test/glib/run-test.sh
+++ test/glib/run-test.sh
@@ -6,11 +6,13 @@ MODE=$1
 ## so the tests can complain if you fail to use the script to launch them
 export DBUS_TEST_GLIB_RUN_TEST_SCRIPT=1
 
+export DBUS_TOP_SRCDIR=$(dirname "$0")/../..
+
 # Rerun ourselves with tmp session bus if we're not already
 if test -z "$DBUS_TEST_GLIB_IN_RUN_TEST"; then
   DBUS_TEST_GLIB_IN_RUN_TEST=1
   export DBUS_TEST_GLIB_IN_RUN_TEST
-  exec $DBUS_TOP_BUILDDIR/tools/run-with-tmp-session-bus.sh $SCRIPTNAME $MODE
+  exec $DBUS_TOP_SRCDIR/tools/run-with-tmp-session-bus.sh $SCRIPTNAME $MODE
 fi  
 
 if test x$MODE = xprofile ; then
