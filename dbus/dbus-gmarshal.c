0f320c5224fe39df44648ac75ecce9e0a01079d0
Simon McVittie <simon.mcvittie@collabora.co.uk>
2011-05-30 15:41:01 +0100

    Include config.h in generated C files, and regenerate dbus-gmarshal.c
    Bug: https://bugs.freedesktop.org/show_bug.cgi?id=37060
    Reviewed-by: Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>

--- dbus/dbus-gmarshal.c
+++ dbus/dbus-gmarshal.c
@@ -1,3 +1,4 @@
+#include <config.h>
 #include "dbus-gmarshal.h"
 
 #include	<glib-object.h>
@@ -22,6 +23,7 @@
 #define g_marshal_value_peek_boxed(v)    g_value_get_boxed (v)
 #define g_marshal_value_peek_pointer(v)  g_value_get_pointer (v)
 #define g_marshal_value_peek_object(v)   g_value_get_object (v)
+#define g_marshal_value_peek_variant(v)  g_value_get_variant (v)
 #else /* !G_ENABLE_DEBUG */
 /* WARNING: This code accesses GValues directly, which is UNSUPPORTED API.
  *          Do not access GValues directly in your code. Instead, use the
@@ -45,16 +47,17 @@
 #define g_marshal_value_peek_boxed(v)    (v)->data[0].v_pointer
 #define g_marshal_value_peek_pointer(v)  (v)->data[0].v_pointer
 #define g_marshal_value_peek_object(v)   (v)->data[0].v_pointer
+#define g_marshal_value_peek_variant(v)  (v)->data[0].v_pointer
 #endif /* !G_ENABLE_DEBUG */
 
 
 /* NONE:STRING,STRING,STRING (dbus-gmarshal.list:1) */
 void
 _dbus_g_marshal_VOID__STRING_STRING_STRING (GClosure     *closure,
-                                            GValue       *return_value,
+                                            GValue       *return_value G_GNUC_UNUSED,
                                             guint         n_param_values,
                                             const GValue *param_values,
-                                            gpointer      invocation_hint,
+                                            gpointer      invocation_hint G_GNUC_UNUSED,
                                             gpointer      marshal_data)
 {
   typedef void (*GMarshalFunc_VOID__STRING_STRING_STRING) (gpointer     data1,
