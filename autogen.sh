0215b0af4c95c9b9fc79ada5b99882f1261e2644
Colin Walters <walters@verbum.org>
2012-01-08 11:28:10 -0500

    autogen.sh: Honor NOCONFIGURE=1 environment variable
    http://people.gnome.org/~walters/docs/build-api.txt

--- autogen.sh
+++ autogen.sh
@@ -140,16 +140,20 @@ autoreconf --install || exit 1
 DIE=0
 cd $ORIGDIR
 
-run_configure=true
-for arg in $*; do
-    case $arg in 
-        --no-configure)
-            run_configure=false
-            ;;
-        *)
-            ;;
-    esac
-done
+if test -z "$NOCONFIGURE"; then
+    run_configure=true
+    for arg in $*; do
+	case $arg in 
+            --no-configure)
+		run_configure=false
+		;;
+            *)
+		;;
+	esac
+    done
+else
+    run_configure=false
+fi
 
 if $run_configure; then
     if test -z "$*"; then
