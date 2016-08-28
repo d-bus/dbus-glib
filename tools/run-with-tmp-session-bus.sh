0e641b27a1e6ea7063a338e00a3de6ed38e3b1d9
Simon McVittie <smcv@debian.org>
2016-08-28 15:12:16 +0100

    run-with-tmp-session-bus.sh: use dbus-run-session
    Signed-off-by: Simon McVittie <smcv@debian.org>

--- tools/run-with-tmp-session-bus.sh
+++ tools/run-with-tmp-session-bus.sh
@@ -45,25 +45,7 @@ export PATH
 unset DBUS_SESSION_BUS_ADDRESS
 unset DBUS_SESSION_BUS_PID
 
-echo "Running dbus-launch --sh-syntax --config-file=$CONFIG_FILE" >&2
-
-eval `dbus-launch --sh-syntax --config-file=$CONFIG_FILE`
-
-if test -z "$DBUS_SESSION_BUS_PID" ; then
-    die "Failed to launch message bus for introspection generation to run"
-fi
-
-echo "Started bus pid $DBUS_SESSION_BUS_PID at $DBUS_SESSION_BUS_ADDRESS" >&2
-
 # Execute wrapped script
-echo "Running $WRAPPED_SCRIPT $@" >&2
-$WRAPPED_SCRIPT "$@" || die "script \"$WRAPPED_SCRIPT\" failed"
-
-kill -TERM $DBUS_SESSION_BUS_PID || die "Message bus vanished! should not have happened" && echo "Killed daemon $DBUS_SESSION_BUS_PID" >&2
-
-sleep 2
-
-## be sure it really died 
-kill -9 $DBUS_SESSION_BUS_PID > /dev/null 2>&1 || true
-
-exit 0
+echo "Running dbus-run-session --config-file=$CONFIG_FILE -- $WRAPPED_SCRIPT $@" >&2
+dbus-run-session --config-file="$CONFIG_FILE" -- $WRAPPED_SCRIPT "$@"
+exit $?
